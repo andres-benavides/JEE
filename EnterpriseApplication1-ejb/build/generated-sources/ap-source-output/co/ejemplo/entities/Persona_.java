c07715777bf2ee5d48a5a1e1cf59928d90dcb912
Nestor Eduardo Guerrero <shadowlord444@gmail.com>
2015-10-02 12:26:33 -0500

    first commit

---
+++ EnterpriseApplication1-ejb/build/generated-sources/ap-source-output/co/ejemplo/entities/Persona_.java
@@ -0,0 +1,14 @@
+package co.ejemplo.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-02T11:02:14")
+@StaticMetamodel(Persona.class)
+public class Persona_ { 
+
+    public static volatile SingularAttribute<Persona, Integer> id;
+    public static volatile SingularAttribute<Persona, String> nombre;
+
+}
---
+++ EnterpriseApplication1-war/src/java/co/ejemplo/servlet/ejemplo.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.ejemplo.servlet;
+
+import co.ejemplo.session.PersonaFacade;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Diseno7
+ */
+
+public class ejemplo extends HttpServlet {
+
+    @EJB
+    PersonaFacade miPersonaSession;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String erro = "No Errors";
+        try {
+            miPersonaSession = (PersonaFacade) new InitialContext().lookup("module/PersonaFacade");
+        } catch (NamingException ex) {
+            Logger.getLogger(ejemplo.class.getName()).log(Level.SEVERE, null, ex);
+            erro = ex.getMessage();
+        }
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ejemplo</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3>Error " + erro+ "</h3>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
